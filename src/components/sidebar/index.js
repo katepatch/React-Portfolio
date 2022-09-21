5ea80cd4dd7be23bdc9c1e9d798c61190c7b1c67
kate patch <kate.epatch@gmail.com>
2022-09-20 18:03:32 -0600

    added logo pics i made

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,5 +1,16 @@
 import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/katelogo1.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
-const Sidebar = () => (<>Kate's Sidebar</>)
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo"/>
+            <img className="sub-logo" src={LogoSubtitle} alt="logosub"/>
+        </Link>
+        
+    </div>
+)
 
 export default Sidebar
