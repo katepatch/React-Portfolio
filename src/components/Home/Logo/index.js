49c0c0d2fca2b8b0d8e1407972a834373a84d674
kate patch <kate.epatch@gmail.com>
2022-09-21 20:19:39 -0600

    working on home page and adding cool animations..  also starting the logo files and putting in a place holder for now

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,6 @@
+import './index.scss';
+import LogoK from '../../../assets/images/ktitle.png';
+
+const Logo = () => 'Hi There'
+
+export default Logo
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,40 @@
+import { useEffect, useState } from 'react'
 import LogoTitle from '../../assets/images/ktitle.png';
 import { Link } from 'react-router-dom';
+//import Loader from 'react-loaders'
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a', 't', 'e']
+    const jobArray = ['w', 'e', 'b', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
 
+    useEffect(() => {
+        return setLetterClass(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hiya, <br /> My name is
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span>
+                <span className={`${letterClass} _13`}>y</span>
+                <span className={`${letterClass} _14`}>a,</span>    
+                <br /> 
+                <span className={`${letterClass} _15`}>I</span>
+                <span className={`${letterClass} _16`}>'m</span>
                 <img src={LogoTitle} alt="developer" />
-                ate
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
                 <br />
-                web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                idx={15} /> 
                 </h1>
                 <h2>Full Stack</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
