57f482fbb048d642e5b0882b7b66825759ceaee5
kate patch <kate.epatch@gmail.com>
2022-09-28 12:02:17 -0600

    i think i figured out the client aspect but i am trying to figure out the image

--- src/components/Home/Selfie/index.js
+++ src/components/Home/Selfie/index.js
@@ -1,16 +1,17 @@
-import './index.scss';
-import Selfie from '../../../assets/images/selfie.jpeg';
-
+import "./index.scss";
+import Selfie from "../../../assets/images/selfie.png";
 
 Selfie = () => {
+  return (
+    <div className="selfie-container">
+      <img
+        className="selfie-pic"
+        src={Selfie}
+        alt="Me trying to show off my curly hair"
+      />
+      <p>This is me. After a day in the ocean.</p>
+    </div>
+  );
+};
 
-
-    return (
-      <div className="selfie-container">
-        <img className="solid-selfie" src={Selfie} alt="Me trying to show off my curly hair" width='400' height='609' />
-          <p>This is me. After a day in the ocean.</p>
-      </div>
-    );
-}
-
-export default Selfie
+export default Selfie;
