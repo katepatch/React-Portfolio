d358970db5da9750b86fedd75ef198bc627db593
kate patch <kate.epatch@gmail.com>
2022-09-29 15:31:31 -0600

    index.js again

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElemntById('root')
 );
 
