bbd67c18e97dfd292bf315d112dd5c0e733f0a71
kate patch <kate.epatch@gmail.com>
2022-09-30 17:41:44 -0600

    only background is on ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Layout from './components/Layouts'
 import './App.scss';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
