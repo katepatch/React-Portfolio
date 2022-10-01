76100f5f39685b2886631f1898a4970cf2141391
kate patch <kate.epatch@gmail.com>
2022-10-01 00:56:37 -0600

    im gonna let the portfolio aspect be done for now and move on

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Layout from './components/Layouts'
 import './App.scss';
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -12,14 +12,14 @@ import Portfolio from './components/Portfolio';
 function App() {
   return (
     <div>
-     <Router basename="React-Portfolio">
+     <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="portfolio" element={<Portfolio />} />
           <Route path="contact" element={<Contact />} />
         </Route>
-     </Router>
+     </Routes>
     </div>
   );
 }
