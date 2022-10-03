f4c3c9082ec5e0738da72c92401f477e3b57fec7
kate patch <kate.epatch@gmail.com>
2022-10-02 21:22:39 -0600

    debugged routes

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
       <Sidebar />
      <Routes>
         <Route exact path="/" element={<Layout />}>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="about" element={<About />} />
-        <Route exact path="portfolio" element={<Portfolio />} />
-        <Route exact path="resume" element={<Resume />} />
-        <Route exact path="contact" element={<Contact />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="about" element={<About />} />
+          <Route exact path="portfolio" element={<Portfolio />} />
+          <Route exact path="resume" element={<Resume />} />
+          <Route exact path="contact" element={<Contact />} />
         </Route>
      </Routes>
      </BrowserRouter>
