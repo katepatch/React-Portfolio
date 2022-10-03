768572cad71b423268e46fc7baac04f63dc3d9c7
kate patch <kate.epatch@gmail.com>
2022-10-02 19:29:47 -0600

    trying to figure out the routes

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   return (
     <div>
      <Routes>
-        <Route path="/React-Portfolio" element={<Layout />}>
+        <Route exact path="/React-Portfolio" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="portfolio" element={<Portfolio />} />
