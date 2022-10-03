00e250767fcbe6437f622a26e3633d8d5fd1ad56
kate patch <kate.epatch@gmail.com>
2022-10-02 20:29:38 -0600

    debugging routes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
 import Layout from './components/Layouts'
 import './App.scss';
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Portfolio from './components/Portfolio';
 import Resume from './components/Resume';
-
-// function App() {
-//   const [page] = useState(['home', 'about', 'contact', 'portfolio', 'resume'])
-//   const [currentPage, setCurrentPage] = useState(page[0]);
-
-//   function renderElement(currentPage) {
-//     switch(currentPage) {
-//       case 'home':
-//         return <Home />;
-//       case 'about':
-//         return <About />;
-//       case 'contact':
-//         return <Contact />;
-//       case 'portfolio':
-//         return <Portfolio />;
-//       case 'resume':
-//         return <Resume />;
-//       default: 
-//         return <Home />;
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <Sidebar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-//       <main>
-//         {renderElement(currentPage)}
-//       </main>
-//     </div>
-//   )
-
-// }
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Sidebar />
      <Routes>
-        <Route exact path="/React-Portfolio" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="resume" element={<Resume />} />
-          <Route path="contact" element={<Contact />} />
+        <Route exact path="/" element={<Layout />}>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="about" element={<About />} />
+        <Route exact path="portfolio" element={<Portfolio />} />
+        <Route exact path="resume" element={<Resume />} />
+        <Route exact path="contact" element={<Contact />} />
         </Route>
      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
